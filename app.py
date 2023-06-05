dce11b286ab4606fdc9e1b25a67e9074c46fbd7a
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2023-06-05 14:24:37 -0500

    app.py

--- app.py
+++ app.py
@@ -17,15 +17,18 @@ Equipo de trabajo: Aura Luz Moreno Díaz, Marcelo Lemus, Verónica Andrea Morale
 """
 
 #Carga de las librerias necesarias para la ejecución del código
+
 import streamlit as st
 import pandas as pd
 import pydeck as pdk #Mapas avanzados
+import plotly as pl
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import base64
 import numpy as np
 from PIL import Image
+
 
 ###Hemos montado los archivos de la base de datos a un hosting para poder trabajar los 3. Modificamos los permisos y se puede acceder a los datos desde cualquier lugar, de esta manera cualquier usuario puede ejecutar la BD desde cualquier parte."""
 
