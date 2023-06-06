5aa06119657d3c3f0a31efd8479929954c2b3949
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2023-06-06 11:07:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,13 @@ st.markdown("<h6 style='text-align: left; color: #990000; font-family: helvetica
 im1, im2, im3 = st.columns((1,1,1)) 
 
 im1_image= "inundaciones.jpg"
-im1.image(im1_image, caption="Incendio residencial. Tomado de: Tomado de: https://media.istockphoto.com/id/1356603199/es/foto/inundaci%C3%B3n-y-humo-negro-del-fuego-en-la-ciudad-y-las-tierras-de-cultivo-despu%C3%A9s-de-la-tormenta.jpg?s=612x612&w=0&k=20&c=Ep9sSC__XJVVePWa1eXCU7fyVLGjb8qBZVO2nuZ1mGc=", width=400, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+im1.image(im1_image, caption="Inundación. Imagen de istockphoto.com", width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+im2_image= "tormentas.jpg"
+im2.image(im2_image, caption="Tormenta. Imagen de istockphoto.com", width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+im3_image= "incendiosCanada.jpg"
+im3.image(im3_image, caption="Incendio. Imagen de istockphoto.com", width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 c1, c2, c3 = st.columns((1,1,1)) 
 
